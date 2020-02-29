8fc9189a697966e4515cadfd27d92cd7e37c4e16
Barton NIU <barton.niu@gmail.com>
2020-03-01 10:58:15 +1100

    Tiny modfication of Bannder

--- components/banner.js
+++ components/banner.js
@@ -1,33 +1,72 @@
 import React from 'react'
-import {Carousel} from 'antd'
+import {Carousel, Row, Button} from 'antd'
 
 
 const Banner =()=>(
-    <div>
-       <Carousel effect="fade"  className="banner-container">
-           <div >
-               <img src="https://www.istockphoto.com/resources/images/HomePage/Hero/985553596.jpg"/>
-               
-           </div>
-           <div>
-           <img src="https://www.istockphoto.com/resources/images/HomePage/Hero/530842059.jpg"/>
-           </div>
+    < >
+         
+       <Carousel effect="fade" className="banner-container" >
+       {/* <Row type="flex" justify="center"> */}
+            <div className="banner-button-slide-1">
+            <button className="banner-button">Find out my work</button>
+                
+            </div>
+            <div className="banner-button-slide-2">
+            <button className="banner-button">Enjoy the data</button>
+           
+            </div>
+         
+            {/* </Row> */}
            </Carousel> 
+           
            <style jsx>{`
-                div {
+                div{
                     width:100%;
                     height:400px;
+                    text-align:center;
+                   
+                    
                 }
                 div img{
                     object-fit: contain;
                 }
+
+                div.banner-button-slide-1{
+                    padding-top:300px;
+                    background-image: url("https://www.istockphoto.com/resources/images/HomePage/Hero/985553596.jpg")
+                }
+
+                div.banner-button-slide-2{
+                    padding-top:300px;
+                    background-image: url("https://www.istockphoto.com/resources/images/HomePage/Hero/530842059.jpg")
+                }
+
                 // .banner-container{
-                //     height:300px;
+                //     display: flex;
+                //     justify-content: center;
+                //     align-items:center;
+                //     background: #1e90ff;;                                  
                 // }
+
+                .banner-button{
+                    font-size:1rem;
+                    color:#ffffff ;
+                    text-transform: uppercase;
+                    background: rgba(0, 0, 0, 0.60);
+                    box-shadow: 5px 5px 5px #888888;
+                    border: none; 
+                    padding:6px 15px;     
+                }
+
+                .banner-button:hover{
+                    cursor:pointer;
+                    background: rgba(0, 0, 0, 0.80);
+                }
+
            `}
 
            </style>
-    </div>
+    </>
 )
 
 
